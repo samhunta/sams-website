c94920d5b6b25917cff1edb032340bce16d3f6f1
Sam Hunter <sam@email.ly>
2016-06-10 23:47:08 -0700

    Refactor `Navigation` component to stateless

--- src/containers/AppContainer/Navigation.js
+++ src/containers/AppContainer/Navigation.js
@@ -2,41 +2,31 @@
 import React, { Component } from 'react'
 import { Link } from 'react-router'
 
-export default class Navigation extends Component {
-  static propTypes = {
-    name: React.PropTypes.string,
-  }
+const Navigation = () => (
+  <div className="site-navigation">
+    <ul>
+      <li>
+        <Link to="/">
+          <strong>Home</strong>
+        </Link>
+      </li>
+      <li>
+        <Link to="/guestbook">Sign my Guestbook</Link>
+      </li>
+      <li>
+        <Link to="/recent-work">Recent work</Link>
+      </li>
+      <li>
+        <Link to="/resume">Resume</Link>
+      </li>
+      <li>
+        <Link to="/contact">Contact me</Link>
+      </li>
+      <li>
+        <a href="https://github.com/samhunta/sams-website">Github</a>
+      </li>
+    </ul>
+  </div>
+)
 
-  constructor(props, context) {
-    super(props, context)
-  }
-
-  render() {
-    return (
-      <div className="site-navigation">
-        <ul>
-          <li>
-            <Link to="/">
-              <strong>Home</strong>
-            </Link>
-          </li>
-          <li>
-            <Link to="/guestbook">Sign my Guestbook</Link>
-          </li>
-          <li>
-            <Link to="/recent-work">Recent work</Link>
-          </li>
-          <li>
-            <Link to="/resume">Resume</Link>
-          </li>
-          <li>
-            <Link to="/contact">Contact me</Link>
-          </li>
-          <li>
-            <a href="https://github.com/samhunta/sams-website">Github</a>
-          </li>
-        </ul>
-      </div>
-    )
-  }
-}
+export default Navigation
